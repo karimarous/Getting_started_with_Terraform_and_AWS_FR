ad106d9b3e4611db73178a1bc4011df5ca56bd51
karimarous <karimarous@gmail.com>
2025-06-09 22:34:59 +0200

    add labs v1

--- lab14/outputs.tf
+++ lab14/outputs.tf
@@ -1,11 +1,3 @@
-output "security_group_id" {
-  value = aws_security_group.security_group.id
-}
-
-output "security_group_name" {
-  value = aws_security_group.security_group.name
-}
-
 output "instance_id" {
   value = aws_instance.instance.*.id
 }
